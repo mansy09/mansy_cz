8af3382024e2209082f3fc97a454a879d480d269
mansy09 <ahmed.elmansy@zyda.com>
2022-04-10 15:03:22 +0200

    Adding package file [initial commit]

---
+++ cz_zyda.py
@@ -0,0 +1,200 @@
+import os
+import re
+
+from commitizen import defaults
+from commitizen.cz.base import BaseCommitizen
+from commitizen.defaults import Questions
+from commitizen.cz.utils import multiple_line_breaker, required_validator
+
+__all__ = ["ConventionalCommitsCz"]
+
+def parse_scope(text):
+    if not text:
+        return ""
+
+    scope = text.strip().split()
+    if len(scope) == 1:
+        return scope[0]
+
+    return "-".join(scope)
+
+
+def parse_subject(text):
+    if isinstance(text, str):
+        text = text.strip(".").strip()
+
+    return required_validator(text, msg="Subject is required.")
+    
+class ZydaCz(BaseCommitizen):
+    # Questions = Iterable[MutableMapping[str, Any]]
+    # It expects a list with dictionaries.
+    def questions(self) -> Questions:
+        """Questions regarding the commit message."""
+        questions = [
+            {
+                "type": "list",
+                "name": "prefix",
+                "message": "Select the type of change you are committing",
+                "choices": [
+                    {
+                        "value": "init",
+                        "name": "init: Initial commits, like creating a new project",
+                        "key": "i",
+                    },
+                    {
+                        "value": "fix",
+                        "name": "fix: A bug fix",
+                        "key": "x",
+                    },
+                    {
+                        "value": "feat",
+                        "name": "feat: Add a new feature",
+                        "key": "f",
+                    },
+                    {
+                        "value": "docs",
+                        "name": "docs: Documentation only changes",
+                        "key": "d",
+                    },
+                    {
+                        "value": "style",
+                        "name": (
+                            "style: Changes that do not affect the "
+                            "meaning of the code (white-space, formatting,"
+                            " missing semi-colons, etc)"
+                        ),
+                        "key": "s",
+                    },
+                    {
+                        "value": "refactor",
+                        "name": (
+                            "refactor: A code change that neither fixes "
+                            "a bug nor adds a feature"
+                        ),
+                        "key": "r",
+                    },
+                    {
+                        "value": "test",
+                        "name": (
+                            "test: Adding missing or correcting " "existing tests"
+                        ),
+                        "key": "t",
+                    },
+                    {
+                        "value": "chore",
+                        "name": (
+                            "chore: updating build tasks, package manager configs, etc; no production code change"
+                        ),
+                        "key": "c",
+                    },
+                    {
+                        "value": "release",
+                        "name": (
+                            "release: release a new version of the project"
+                            "scripts (example scopes: GitLabCI)"
+                        ),
+                        "key": "z",
+                    },
+                ],
+            },
+            {
+                "type": "input",
+                "name": "scope",
+                "message": (
+                    "What is the scope of this change? (class or file name): (press [enter] to skip)\n"
+                ),
+                "filter": parse_scope,
+            },
+            {
+                "type": "input",
+                "name": "subject",
+                "filter": parse_subject,
+                "message": (
+                    "Write a short and imperative summary of the code changes: (lower case and no period)\n"
+                ),
+            },
+            {
+                "type": "input",
+                "name": "body",
+                "message": (
+                    "Provide additional contextual information about the code changes: (press [enter] to skip)\n"
+                ),
+                "filter": multiple_line_breaker,
+            },
+            {
+                "type": "confirm",
+                "message": "Is this a BREAKING CHANGE? Correlates with MAJOR in SemVer",
+                "name": "is_breaking_change",
+                "default": False,
+            },
+            {
+                "type": "input",
+                "name": "footer",
+                "message": (
+                    "Footer. Information about Breaking Changes and "
+                    "reference issues that this commit closes: (press [enter] to skip)\n"
+                ),
+            },
+        ]
+        return questions
+
+    def message(self, answers: dict) -> str:
+        prefix = answers["prefix"]
+        scope = answers["scope"]
+        subject = answers["subject"]
+        body = answers["body"]
+        footer = answers["footer"]
+        is_breaking_change = answers["is_breaking_change"]
+
+        if scope:
+            scope = f"{scope}"
+        if body:
+            body = f"\n\n{body}"
+        if is_breaking_change:
+            footer = f"BREAKING CHANGE: {footer}"
+        if footer:
+            footer = f"\n\n{footer}"
+
+        message = f"{scope}[{prefix}]: {subject}{body}{footer}"
+
+        return message
+
+    def example(self) -> str:
+        """Provide an example to help understand the style (OPTIONAL)
+
+        Used by `cz example`.
+        """
+        return 'Problem with user (#321)'
+
+    def schema(self) -> str:
+        return (
+            "<type>(<scope>): <subject>\n"
+            "<BLANK LINE>\n"
+            "<body>\n"
+            "<BLANK LINE>\n"
+            "(BREAKING CHANGE: )<footer>"
+        )
+
+    def schema_pattern(self) -> str:
+        PATTERN = (
+            r"(chore|docs|feat|fix|init|refactor|release|style|test)"
+            r"(\(\S+\))?!?:(\s.*)"
+        )
+        return PATTERN
+
+    def info(self) -> str:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        filepath = os.path.join(dir_path, "conventional_commits_info.txt")
+        with open(filepath, "r") as f:
+            content = f.read()
+        return content
+    
+    def process_commit(self, commit: str) -> str:
+        pat = re.compile(self.schema_pattern())
+        m = re.match(pat, commit)
+        if m is None:
+            return ""
+        return m.group(3).strip()
+
+
+discover_this = ZydaCz  # used by the plug-in system
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ZydaCommitizen',
+    version='0.1.0',
+    py_modules=['cz_zyda'],
+    license='MIT',
+    long_description='this is a long description',
+    install_requires=['commitizen']
+)
